0c5a58a39b57950dbc969dcdee253b0730149ea8
lwande01-nm <lwandE01@icloud.com>
2026-05-08 10:39:48 +0200

    fix

--- Exercise1-Variables/obj/Debug/net10.0/Exercise1-Variables.AssemblyInfo.cs
+++ Exercise1-Variables/obj/Debug/net10.0/Exercise1-Variables.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise1-Variables")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+961f42ac20708def5965ff30a985c4ec59ab1a27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00def04fd1553432bd72e8d9617303e65112e578")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise1-Variables")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise1-Variables")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
